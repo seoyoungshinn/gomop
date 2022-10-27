66f0f57237ff6b06879fc6c0b54bf2120343af8d
seoyoungshinn <doddo1225@gmail.com>
2022-10-27 15:34:42 +0900

    회원가입구현성공
    아이디형식 : testn@test.com
    (n은 정수)
    비번 : test123으로 통일

--- app/src/main/java/com/example/gomop/MainActivity.kt
+++ app/src/main/java/com/example/gomop/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.gomop
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.gomop.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        startActivity(Intent(this, SignUpActivity::class.java))
+
     }
 }
---
+++ app/src/main/java/com/example/gomop/SignUpActivity.kt
@@ -0,0 +1,74 @@
+package com.example.gomop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.gomop.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+/*class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}*/
+
+class SignUpActivity : AppCompatActivity() {
+
+    private var _binding: ActivitySignUpBinding? = null
+    private val binding get() = _binding!!
+
+    //private val binding = ActivitySignUpBinding.inflate(layoutInflater)
+
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+        Log.d("auth",auth.toString())
+
+        binding.btnSignIn.setOnClickListener {
+            val email = binding.edtEmail.text.toString().trim()
+            val password = binding.edtPassword.text.toString().trim()
+
+            // Validate...
+
+            createUser(email, password)
+        }
+    }
+
+    private fun createUser(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "회원가입 성공", Toast.LENGTH_SHORT).show()
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    Toast.makeText(this, "회원가입 실패", Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "회원가입 실패", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        user?.let {
+            binding.txtResult.text = "Email: ${user.email}\nUid: ${user.uid}"
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
